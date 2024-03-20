def3ce79025d6a61c26022f8d2922df2edf732bd
MADElinessss <jyseen@naver.com>
2024-03-20 10:03:35 +0900

    [Feat] NumberSelectView 번호 선택 구현

--- LottoDiary/View/MainGroup/MyNumber/MyNumberViewController.swift
+++ LottoDiary/View/MainGroup/MyNumber/MyNumberViewController.swift
@@ -8,43 +8,63 @@
 import SnapKit
 import UIKit
 
-// TODO: UI 구현
 final class MyNumberViewController: BaseViewController {
     
-    let tableView = UITableView()
-    
-//    var no1: Int = 0
-//    var no2: Int = 0
-//    var no3: Int = 0
-//    var no4: Int = 0
-//    var no5: Int = 0
-//    var no6: Int = 0
+    private let tableView = UITableView()
+    private let stackView = UIStackView()
+    private var selectedButtons: [UIButton] = []
+    private let memoTextField = UITextField()
+    private let numberSelectionView = NumberSelectionView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureTableView()
+        setupView()
+        setupConstraints()
+        setupButtonActions()
     }
     
-    override func configureHierarchy() {
+    private func configureTableView() {
         view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.separatorStyle = .none
+        tableView.tableFooterView = UIView()
+        
     }
     
-    override func configureLayout() {
-        tableView.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide)
-            make.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide)
+    private func setupButtonActions() {
+        for (index, button) in selectedButtons.enumerated() {
+            button.tag = index
+            button.addTarget(self, action: #selector(selectedNumberButtonTapped(_:)), for: .touchUpInside)
         }
     }
     
-    override func configureView() {
+    
+    private func setupView() {
+        view.backgroundColor = .white
+        
         let leftButton = createBarButtonItem(imageName: "chevron.left", action: #selector(leftButtonTapped))
         let rightButton = UIBarButtonItem(title: "저장", style: .done, target: self, action: #selector(rightButtonTapped))
         configureNavigationBar(title: "나의 번호 편집", leftBarButton: leftButton, rightBarButton: rightButton)
         
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.register(MyNumberTableViewCell.self, forCellReuseIdentifier: "MyNumberTableViewCell")
+        
+        setupStackView()
+        setupNumberSelectionView()
     }
+    
+    @objc private func selectedNumberButtonTapped(_ sender: UIButton) {
+        guard let title = sender.title(for: .normal), let numberToRemove = Int(title) else { return }
+        
+        numberSelectionView.selectedNumbers.removeAll { $0 == numberToRemove }
+        updateSelectedNumbers(numberSelectionView.selectedNumbers)
+        numberSelectionView.updateUI()
+    }
+    
     
     @objc func leftButtonTapped() {
         navigationController?.popViewController(animated: true)
@@ -52,6 +72,64 @@ final class MyNumberViewController: BaseViewController {
     
     @objc func rightButtonTapped() {
         // TODO: Realm 저장
+    }
+    
+    
+    private func setupStackView() {
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 10
+        view.addSubview(stackView)
+        
+        for _ in 0..<6 {
+            let button = UIButton()
+            button.backgroundColor = .lightGray
+            button.setTitleColor(.black, for: .normal)
+            button.layer.cornerRadius = 25
+            selectedButtons.append(button)
+            stackView.addArrangedSubview(button)
+        }
+    }
+    
+    private func setupNumberSelectionView() {
+        numberSelectionView.onNumberSelected = { [weak self] selectedNumbers in
+            self?.updateSelectedNumbers(selectedNumbers)
+        }
+        view.addSubview(numberSelectionView)
+    }
+    
+    private func setupConstraints() {
+        stackView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.centerX.equalTo(view)
+            make.height.equalTo(50)
+        }
+        
+        numberSelectionView.snp.makeConstraints { make in
+            make.top.equalTo(stackView.snp.bottom).offset(20)
+            make.left.right.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        selectedButtons.forEach { button in
+            button.snp.makeConstraints { make in
+                make.width.equalTo(button.snp.height)
+            }
+        }
+    }
+    
+    private func updateSelectedNumbers(_ numbers: [Int]) {
+        selectedButtons.forEach {
+            $0.setTitle("", for: .normal)
+            $0.backgroundColor = .lightGray
+        }
+        
+        for (index, number) in numbers.enumerated() {
+            if index < selectedButtons.count {
+                let button = selectedButtons[index]
+                button.setTitle("\(number)", for: .normal)
+                button.backgroundColor = color(for: number)
+            }
+        }
     }
     
     private func color(for drawNumber: Int) -> UIColor {
@@ -67,7 +145,7 @@ final class MyNumberViewController: BaseViewController {
         case 41...45:
             return UIColor(named: "lotteryGreen") ?? .green
         default:
-            return .gray // 기본값으로 검정색 사용
+            return .lightGray
         }
     }
 }
@@ -82,19 +160,43 @@ extension MyNumberViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 500
+        if indexPath.section == 0 {
+            return 300
+        } else {
+            return 100
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        if indexPath.section == 0 {
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "MyNumberTableViewCell", for: indexPath) as! MyNumberTableViewCell
-//            
-//            return cell
-//        }
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MyNumberTableViewCell", for: indexPath) as! MyNumberTableViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.selectionStyle = .none
+        
+        if indexPath.section == 0 {
+            // 번호 선택 섹션
+            setupNumberSelectionView()
+            cell.contentView.addSubview(numberSelectionView)
+            numberSelectionView.snp.makeConstraints { make in
+                make.edges.equalToSuperview()
+            }
+        } else if indexPath.section == 1 {
+            // 번호 메모 섹션
+            setupMemoTextField()
+            cell.contentView.addSubview(memoTextField)
+            memoTextField.snp.makeConstraints { make in
+                make.edges.equalToSuperview().inset(16)
+            }
+        }
         
         return cell
     }
     
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "번호 선택" : "번호 메모"
+    }
     
+    private func setupMemoTextField() {
+        let number: Int = 1
+        memoTextField.placeholder = "나의 번호(\(number)"
+        memoTextField.borderStyle = .roundedRect
+    }
 }
--- LottoDiary/View/MainGroup/MyNumber/NumberButtons/NumberSelectionView.swift
+++ LottoDiary/View/MainGroup/MyNumber/NumberButtons/NumberSelectionView.swift
@@ -14,13 +14,19 @@ class NumberSelectionView: UIView {
         case main
     }
     
+    // 선택된 번호가 변경될 때 호출할 Closure
+    var onNumberSelected: (([Int]) -> Void)?
+    
+    // 이전에 선택한 번호를 관리
+    var selectedNumbers: [Int] = []
+    
+    
     var dataSource: UICollectionViewDiffableDataSource<Section, Int>! = nil
     var collectionView: UICollectionView! = nil
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .orange
         configureHierarchy()
         configureDataSource()
     }
@@ -29,6 +35,37 @@ class NumberSelectionView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 }
+
+extension NumberSelectionView: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let number = indexPath.item + 1
+
+        if let index = selectedNumbers.firstIndex(of: number) {
+            selectedNumbers.remove(at: index)
+        } else {
+            if selectedNumbers.count < 6 {
+                selectedNumbers.append(number)
+            }
+        }
+
+        selectedNumbers.sort()
+        
+        onNumberSelected?(selectedNumbers)
+        updateUI()
+    }
+
+    func updateUI() {
+        for case let cell as TextCell in collectionView.visibleCells {
+            guard let indexPath = collectionView.indexPath(for: cell),
+                  let number = dataSource.itemIdentifier(for: indexPath) else { continue }
+            
+            cell.layer.borderWidth = selectedNumbers.contains(number) ? 2 : 0
+            cell.layer.borderColor = selectedNumbers.contains(number) ? UIColor(named: "point")?.cgColor : UIColor.clear.cgColor
+        }
+    }
+}
+
+
 
 extension NumberSelectionView {
     /// - Tag: Inset
@@ -39,31 +76,57 @@ extension NumberSelectionView {
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
         
         item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
-
+        
         let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
                                                heightDimension: .fractionalHeight(0.07))
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize,
-                                                         subitems: [item])
+                                                       subitems: [item])
         
         let section = NSCollectionLayoutSection(group: group)
         let layout = UICollectionViewCompositionalLayout(section: section)
         
         return layout
     }
+    
+    func updateUIForNumber(_ number: Int, isSelected: Bool) {
+        guard let indexPath = dataSource.indexPath(for: number) else { return }
+        guard let cell = collectionView.cellForItem(at: indexPath) as? TextCell else { return }
+        
+        cell.layer.borderWidth = isSelected ? 2 : 0
+        cell.layer.borderColor = isSelected ? UIColor(named: "point")?.cgColor : UIColor.clear.cgColor
+    }
 }
 
 extension NumberSelectionView {
     
+//    func configureHierarchy() {
+//        collectionView = UICollectionView(frame: bounds, collectionViewLayout: createLayout())
+//        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+//        collectionView.backgroundColor = .systemBackground
+//        collectionView.delegate = self
+//        addSubview(collectionView)
+//    }
+    
     func configureHierarchy() {
-        collectionView = UICollectionView(frame: bounds, collectionViewLayout: createLayout())
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: createLayout())
         collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         collectionView.backgroundColor = .systemBackground
+        collectionView.delegate = self
         addSubview(collectionView)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: self.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
     }
+
     
     func configureDataSource() {
         let cellRegistration = UICollectionView.CellRegistration<TextCell, Int> { (cell, indexPath, identifier) in
-            // Populate the cell with our item description.
+            
             cell.label.text = "\(identifier)"
             cell.label.font = .systemFont(ofSize: 14, weight: .semibold)
             cell.label.textColor = .white
@@ -76,10 +139,9 @@ extension NumberSelectionView {
         
         dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView) {
             (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
-            // Return the cell.
             return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: identifier)
         }
-
+        
         // initial data
         var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
         snapshot.appendSections([.main])
@@ -105,8 +167,3 @@ extension NumberSelectionView {
     }
 }
 
-//extension NumberSelectionViewController: UICollectionViewDelegate {
-//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-//        <#code#>
-//    }
-//}
